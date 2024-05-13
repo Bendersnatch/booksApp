42bc7616775964d6200e75a5f33e68d19b680272
Sasha <45601549+Bendersnatch@users.noreply.github.com>
2024-05-13 17:16:29 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log(title)
+    books.push({id: 123, title: title})
+    setBooks(books)
   }
 
   return <div>
+    {books.length}
     <BookCreate onCreate={createBook} />
     </div>
 }
